6f053e314988563895eebdcf2affd359e362d0e3
DmytroLys <yellowsoljah95@gmail.com>
2023-03-02 19:16:57 +0100

    Create PlusButton with UIBezierPath

--- Agua/PushButton.swift
+++ Agua/PushButton.swift
@@ -9,11 +9,51 @@ import UIKit
 
 @IBDesignable
 class PushButton: UIButton {
+    
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift = 0.5
+    }
+    
+    private var halfWidth: CGFloat {
+        bounds.width / 2
+    }
+    
+    private var halfHeight: CGFloat {
+        bounds.height / 2
+    }
+    
+    
 
     override func draw (_ rect: CGRect) {
         let path = UIBezierPath(ovalIn: rect)
-        UIColor.green.setFill()
+        UIColor.blue.setFill()
         path.fill()
+        
+        let plusWidth = min(bounds.width, bounds.height) * Constants.plusButtonScale
+        let halfOfPuslWidth = plusWidth / 2
+
+
+        let plusPath = UIBezierPath()
+        plusPath.lineWidth = Constants.plusLineWidth
+
+        plusPath.move(to: CGPoint(x: halfWidth - halfOfPuslWidth, y: halfHeight))
+        plusPath.addLine(to: CGPoint(x: halfWidth + halfOfPuslWidth, y: halfHeight))
+
+        
+        plusPath.move(to: CGPoint(x: halfWidth, y: halfHeight - halfOfPuslWidth))
+        plusPath.addLine(to: CGPoint(x: halfWidth, y: halfHeight + halfOfPuslWidth))
+        
+        UIColor.white.setStroke()
+
+        plusPath.stroke()
+        
+
+        
+        
+        
+        
     }
     
 }
--- Agua/ViewController.swift
+++ Agua/ViewController.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
+@IBDesignable
 class ViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
     }
 
 
 }
+
+
 
