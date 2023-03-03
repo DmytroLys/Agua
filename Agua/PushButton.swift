1f3116d9bd18ec5231dc58db3eb7d78becc2d353
DmytroLys <yellowsoljah95@gmail.com>
2023-03-03 11:17:30 +0100

    add IBInsectable ti button and create delete button

--- Agua/PushButton.swift
+++ Agua/PushButton.swift
@@ -10,6 +10,9 @@ import UIKit
 @IBDesignable
 class PushButton: UIButton {
     
+    @IBInspectable var fillColor : UIColor = .green
+    @IBInspectable var isAddButton: Bool = true
+    
     private struct Constants {
         static let plusLineWidth: CGFloat = 3.0
         static let plusButtonScale: CGFloat = 0.6
@@ -25,33 +28,31 @@ class PushButton: UIButton {
     }
     
     
-
+    
     override func draw (_ rect: CGRect) {
         let path = UIBezierPath(ovalIn: rect)
-        UIColor.blue.setFill()
+        fillColor.setFill()
         path.fill()
         
         let plusWidth = min(bounds.width, bounds.height) * Constants.plusButtonScale
         let halfOfPuslWidth = plusWidth / 2
-
-
+        
+        
         let plusPath = UIBezierPath()
         plusPath.lineWidth = Constants.plusLineWidth
-
+        
         plusPath.move(to: CGPoint(x: halfWidth - halfOfPuslWidth, y: halfHeight))
         plusPath.addLine(to: CGPoint(x: halfWidth + halfOfPuslWidth, y: halfHeight))
-
         
-        plusPath.move(to: CGPoint(x: halfWidth, y: halfHeight - halfOfPuslWidth))
-        plusPath.addLine(to: CGPoint(x: halfWidth, y: halfHeight + halfOfPuslWidth))
+        
+        if isAddButton {
+            plusPath.move(to: CGPoint(x: halfWidth, y: halfHeight - halfOfPuslWidth))
+            plusPath.addLine(to: CGPoint(x: halfWidth, y: halfHeight + halfOfPuslWidth))
+        }
         
         UIColor.white.setStroke()
-
+        
         plusPath.stroke()
-        
-
-        
-        
         
         
     }
