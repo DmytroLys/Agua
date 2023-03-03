493f5c3be9bbd7e25a4f2e3c16823a1d2d23385e
DmytroLys <yellowsoljah95@gmail.com>
2023-03-03 17:40:51 +0100

    Create GraphView with tap gesture transition

---
+++ Agua/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  Agua
+//
+//  Created by Dmytro Lyshtva on 03.03.2023.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
